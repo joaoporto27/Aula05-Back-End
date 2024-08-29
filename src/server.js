843de6467ba92622700b6a8b5c7979ecbbb4997b
João Vitor <joaovitorportosales46@gmail.com>
2024-08-29 15:51:35 -0300

    Feat: Add some requests

--- src/server.js
+++ src/server.js
@@ -9,9 +9,57 @@ const app = express()
 
 app.use(express.json())
 
+const guloseimas = [
+    {
+        id: 1,
+        nome: "Trufa",
+        preco: 8.0,
+    },
+    {
+        id: 2,
+        nome: "Palha italiana",
+        preco: 3.0
+    },
+    {
+        id: 3,
+        nome: "Ninho trufado",
+        preco: 9.0,
+    },
+];
+
+const filmesMarcantes = [
+    {
+        id: 1001,
+        titulo: "Meu Amigo Totoro",
+        genero: "Animação",
+        emCartaz: false,
+    },
+    {
+        id:1002,
+        titulo: "Deadpool 3",
+        genero: "Ação e Comédia",
+        emCartaz: true,
+    },
+    {
+        id:1003,
+        titulo: "Meu Malvado Favorito 4",
+        genero: "Animação",
+        emCartaz: true,
+    }
+]
+
 app.get("/", (req, res) => {
         return res.status(200).send({ message: "Hello, World!"})
 })
+
+app.get("/doces", (req, res) => {
+    return res.status(200).send(guloseimas)
+})
+
+app.get("/filmes", (req, res) => {
+    return res.status(200).send(filmesMarcantes)
+})
+
 
 app.listen(port, () => {
     console.log(`😀 Server started on http://localhost:${port}`    )
