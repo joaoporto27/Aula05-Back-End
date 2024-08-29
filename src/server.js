3a56d7bfeb3c7f8f3632ee1b4a771f6fb1d83e66
João Vitor <joaovitorportosales46@gmail.com>
2024-08-29 17:09:01 -0300

    Refactor: Alter doces routes structures

--- src/server.js
+++ src/server.js
@@ -9,24 +9,6 @@ const app = express()
 
 app.use(express.json())
 
-const guloseimas = [
-    {
-        id: 1,
-        nome: "Trufa",
-        preco: 8.0,
-    },
-    {
-        id: 2,
-        nome: "Palha italiana",
-        preco: 3.0
-    },
-    {
-        id: 3,
-        nome: "Ninho trufado",
-        preco: 9.0,
-    },
-];
-
 const filmesMarcantes = [
     {
         id: 1001,
@@ -52,26 +34,11 @@ app.get("/", (req, res) => {
         return res.status(200).send({ message: "Hello, World!"})
 })
 
-app.get("/doces", (req, res) => {
-    return res.status(200).send(guloseimas)
-})
 
-app.post("/doces", (req, res) => {
-    const {nome, preco} = req.body;
-    const novoDoce = {
-        id: guloseimas.length + 1,
-        nome: nome,
-        preco: preco,
-};
-
-    guloseimas.push(novoDoce)
-    return res.status(201).send(guloseimas)
-})
 
 app.get("/filmes", (req, res) => {
     return res.status(200).send(filmesMarcantes)
 })
-
 
 app.listen(port, () => {
     console.log(`😀 Server started on http://localhost:${port}`    )
