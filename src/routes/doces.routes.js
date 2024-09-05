c9ba77f987fb4caea92989640bf0a81d128a97bc
João Vitor <joaovitorportosales46@gmail.com>
2024-09-05 11:41:25 -0300

    Feat: Create delete method

--- src/routes/doces.routes.js
+++ src/routes/doces.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 const docesRoutes = Router();
 
-const guloseimas = [
+let guloseimas = [
     {
         id: 1,
         nome: "Trufa",
@@ -80,6 +80,23 @@ docesRoutes.put("/:id", (req, res) => {
     });
 });
 
+//Rota para deletar uma guloseima 
+docesRoutes.delete("/:id", (req, res) => {
+    const { id } = req.params;
 
+    const guloseima = guloseimas.find((doce) => doce.id === Number(id));
+
+    
+    if (!guloseima) {
+        return res.status(404).send({ message: "Guloseima não encontrada!" });
+    }
+    
+    guloseimas = guloseimas.filter((doce) => doce.id !== Number(id));
+
+    return res.status(200).send ({
+        message: "Guloseima deletada!",
+        guloseima,
+    });
+});
 
 export default docesRoutes;
