4f167ce62b1106e3f94d048d925819e35383a744
João Vitor <joaovitorportosales46@gmail.com>
2024-09-05 09:52:49 -0300

    Feat: Create get by id method

--- src/routes/doces.routes.js
+++ src/routes/doces.routes.js
@@ -20,20 +20,40 @@ const guloseimas = [
     },
 ];
 
+//Rota para buscar todo os elementos do array guloseimas
 docesRoutes.get("/", (req, res) => {
-    return res.status(200).send(guloseimas)
+    return res.status(200).send(guloseimas);
 })
 
+//Rota para criar uma nova guloseima 
 docesRoutes.post("/", (req, res) => {
-    const {nome, preco} = req.body;
+    const { nome, preco } = req.body;
     const novoDoce = {
         id: guloseimas.length + 1,
         nome: nome,
         preco: preco,
-};
+    };
 
-    guloseimas.push(novoDoce)
-    return res.status(201).send(guloseimas)
+    guloseimas.push(novoDoce);
+    return res.status(201).send(guloseimas);
+})
+
+
+//Rota para buscar um elemento específico do array guloseimas
+docesRoutes.get("/:id", (req, res) => {
+    const { id } = req.params;
+
+    console.log(id);
+
+    const guloseima = guloseimas.find((doce) => doce.id === Number(id));
+
+    console.log(guloseima);
+
+    if (!guloseima) {
+        return res.status(404).send({ message: "Guloseima não encontrada!" });
+    }
+
+    return res.status(200).send(guloseima);
 })
 
 export default docesRoutes;
